45def4bbf5324cd0b38d1ffbdea7a25b8ba5c6fc
minWi <e.minguez@gmail.com>
2012-08-24 11:04:54 +0300

    Update Aire_Acondicionado_y_temperatura.ino
    Comentarios añadidos

--- Aire_Acondicionado_y_temperatura.ino
+++ Aire_Acondicionado_y_temperatura.ino
@@ -8,28 +8,39 @@ int segundos = 0;
 char status='f';
 
 void setup(){
-
+  // Configuracion del serial para coincidir con la fonera
   Serial.begin(9600);
+  // AC = salida
   pinMode(AC, OUTPUT);
+  // Interruptor = entrada
   pinMode(INTERRUPTOR,INPUT);
+  // El AC arranca apagado
   digitalWrite(AC,LOW);
 }
 
 void loop(){
+  // Solamente escribe la temperatura cada 59 segundos + el delay = 1 min
   if(segundos == 59){
-    tempC = analogRead(tempPin);           //read the value from the sensor
-    tempC = (5.0 * tempC * 100.0)/1024.0;  //convert the analog data to temperature
-    Serial.println(tempC);             //send the data to the computer
+    // lee el valor del sensor
+    tempC = analogRead(tempPin);
+    // convierte el valor leido en grados C
+    tempC = (5.0 * tempC * 100.0)/1024.0;
+    // envia la temperatura por el puerto serie conectado a la fonera
+    Serial.println(tempC);
     segundos=0;
   }
   else
   {
     segundos++;
   }
+  
+  // El interruptor tiene prioridad sobre la web
 
+  // Si el interruptor se pulsa, da igual como esté en la web
   if(digitalRead(INTERRUPTOR) && (status == 'f'))
   {
     digitalWrite(AC,HIGH);
+    // Escribe I-ON para distinguir si ha sido desde web o desde el interruptor
     Serial.println("I-ON");
     status='n';
   }
@@ -40,18 +51,23 @@ void loop(){
     status='f';
   }
 
+  // Si no esta pulsado el interruptor
   if(!digitalRead(INTERRUPTOR) && (status == 'f'))
   {
+    // Comprobar el puerto serie
     if(Serial.available()> 0)
     {
+      // En caso de que haya datos, leerlos
       byte_received= Serial.read();
       if(byte_received == 'N')
       {
+        // Si es "N", encender el AC y escribir W-ON por el serial
         digitalWrite(AC,HIGH);
         Serial.println("W-ON");
       }
       if(byte_received == 'F')
       {
+        // Si es "F", apagar el AC y escribir W-OFF por el serial
         digitalWrite(AC,LOW);
         Serial.println("W-OFF");
       }
@@ -59,9 +75,11 @@ void loop(){
   }
   else
   {
+    // Limpieza de buffer
     while (Serial.read() >= 0)
       ;
   }
+  // Esperar 1 segundo
   delay(1000);
 }
 
